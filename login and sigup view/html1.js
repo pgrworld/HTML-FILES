40469607a10867231ea319911b68baca5d3d43ce
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-07-02 18:56:33 +0530

    Create html1.js

---
+++ login and sigup view/html1.js
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+<head>
+<title>SECURIFI EMBEDDED SYSTEMS</title>
+ </head> 
+ <body>
+ <h1> Welcome to Securifi embedded systems </h1>
+ <h4> click on signup button </h4>   
+ <button id="myButton" >signup</button>
+<script type="text/javascript">
+    document.getElementById("myButton").onclick = function () {
+        location.href = "file:///home/securifi/Desktop/html2.html";
+    };
+</script>
+<h4>click on login button</h4> 
+<button id= "myButton1">login</button>
+<script type = "text/javascript">
+   document.getElementById("myButton1").onclick = function(){
+   location.href = "file:///home/securifi/Desktop/html3.html";
+   };
+   </script>
+  </body>
+</html>
