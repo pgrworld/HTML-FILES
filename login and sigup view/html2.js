13a10d0e2df85131240786a0d732eefaf850bcd6
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-07-02 18:56:51 +0530

    Create html2.js

---
+++ login and sigup view/html2.js
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+<title>signup form</title>
+</head> 
+<h3>signup form</h3>
+<body>
+ <form>
+   <table>
+    <tr>
+     <td>
+       username:
+     </td>
+       <td>
+       <input type ="text" username=" " placeholder="username">
+       </td>
+    </tr>
+     <tr>
+      <td>
+      password:
+      </td>
+      <td>
+       <input type ="text" password= " " placeholder= "password">
+      </td>
+     </tr>
+   </table>
+ </form>
+ <button>SIGNUP</button>
+</body>
+</html>
