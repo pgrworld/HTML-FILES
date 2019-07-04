db328148adfa8928fa99c511888f8feab530bec5
pgrworld <51364621+pgrworld@users.noreply.github.com>
2019-07-04 17:22:17 +0530

    Update html1.js

--- login and sigup view/html1.js
+++ login and sigup view/html1.js
@@ -4,19 +4,19 @@
 <title>SECURIFI EMBEDDED SYSTEMS</title>
  </head> 
  <body>
- <h1> Welcome to Securifi embedded systems </h1>
+ <h1 > Welcome to Securifi embedded systems </h1>
  <h4> click on signup button </h4>   
  <button id="myButton" >signup</button>
-<script type="text/javascript">
+<script>
     document.getElementById("myButton").onclick = function () {
-        location.href = "file:///home/securifi/Desktop/html2.html";
+        location.href = "file:///home/securifi/Desktop/html4.html";
     };
 </script>
 <h4>click on login button</h4> 
-<button id= "myButton1">login</button>
-<script type = "text/javascript">
+<button id= "myButton1" >login</button>
+<script>
    document.getElementById("myButton1").onclick = function(){
-   location.href = "file:///home/securifi/Desktop/html3.html";
+   location.href = "file:///home/securifi/Desktop/html5.html";
    };
    </script>
   </body>
